70c8bcadcb297e860b0e69147a5804e49ee3ce1f
kimeberz <kimeberz@gmail.com>
2015-10-05 18:07:54 -0700

    wipn it

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
 });
 
 http.listen(3000, function(){
